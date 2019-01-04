a97e69a23ac78ac3fc9ff9b505d65cea455df034
ssemandah <hs42@students.kiron.ngo>
2019-01-04 12:15:42 +0300

    working api endpoints

--- ireporter/api.py
+++ ireporter/api.py
@@ -1,103 +1,178 @@
-from flask import Blueprint, request, jsonify
-from ireporter.models import *
 from datetime import datetime
+from flask import Blueprint, jsonify, make_response, request, abort
 
+from ireporter.models import *
 
 bp = Blueprint('api', __name__, url_prefix='/api/v1')
 
-users = [{'userId': 1, 'firstname': 'Hassan', 'lastname': 'Ssemanda', 'othernames': 'None',  'email': 'hassan@email.com', 'phonenumber': 256777888999, 'username': 'Hassan', 'isAdmin': True},\
-{'userId': 2, 'firstname': 'Bob', 'lastname': 'Magembe', 'othernames': 'John',  'email': 'bob@email.com', 'phonenumber': 256777444999, 'username': 'Bob', 'isAdmin': False}]
+users = [
+    {
+        'id': 1, 
+        'firstname': 'Hassan', 
+        'lastname': 'Ssemanda', 
+        'othernames': '',  
+        'email': 'hassan@email.com', 
+        'phonenumber': 256777888999, 
+        'username': 'Hassan', 
+        'isAdmin': True,
+        "registered": "Thu, 03 Jan 2019 13:57:26 GMT",
+    },
+    {
+        'id': 2, 
+        'firstname': 'Bob', 
+        'lastname': 'Magembe', 
+        'othernames': 'John',  
+        'email': 'bob@email.com', 
+        'phonenumber': 256777444999, 
+        'username': 'Bob', 
+        'isAdmin': False,
+        "registered": "Thu, 03 Jan 2019 15:05:26 GMT",
+    }
+]
 
+@bp.route('/users', methods=['GET'])
+def get_users():
+    # diaplays all users
+    return jsonify({
+    'status': 200,
+    "users": users
+    }), 200
+
+@bp.route('/users/<int:userId>', methods=['GET'])
+def get_user(userId):
+    user = [user for user in users if user['id'] == userId]    
+    if len(user) == 0:
+        return jsonify({
+            'status': 400,
+            "message": "user id provided is out of range "
+        }), 400
+    return jsonify({
+            'status': 200,
+            'user': user[0]
+        }), 200
+      
+        
 @bp.route('/users', methods=['POST'])
 def register_user():
     # registers a  new user
-    data = request.get_json()
-    firstname = data.get('firstname')
-    lastname = data.get('lastname')
-    othernames = data.get('othernames') 
-    email = data.get('email')
-    phonenumber = data.get('phonenumber')
-    username = data.get('username')
-    isAdmin = data.get('isAdmin')
-
-    user = User(firstname, lastname, othernames,  email, phonenumber, username)
+    if not request.json or not 'username' in request.json:
+        return jsonify({
+            'status': 400,
+            'error': 'Bad Format'}), 400
+    user = {
+        'id': users[-1]['id'] + 1,
+        'firstname': request.json('firstname'),
+        'lastname': request.json('lastname'),
+        'othernames': request.json('othernames') ,
+        'email' : request.json('email'),
+        'phonenumber': request.json('phonenumber'),
+        'username': request.json('username'),
+        'isAdmin': False,
+        'registered': datetime.now()
+    }    
     users.append(user)
     return jsonify({
         'status': 201,
         'data': {
-            'id': data['userId'],
+            'id': user['id'],
             'message': 'User created'
         }
-    })
-
-@bp.route('/users', methods=['GET'])
-def fetch_users():
-    # fetches all users    
-    try:
-        return jsonify({
-        'status': 200,
-        "users": users
-        }), 200
-    except:
-        return jsonify({
-        'status': 404,
-        "users": 'no user found'
-        }), 404
-
-
-@bp.route('/users/<int:userId>', methods=['GET'])
-def get_user_details(userId):
-    for user in users:
-        if user['userId'] == userId:
-            return jsonify({
-                'status': 200,
-                'user': user
-            }), 200
-        return jsonify({
-            'status': 400,
-            "message": "user id provided is out of range "}), 400   
+    }), 201
 
 
 @bp.route('/users/<int:userId>', methods=['DELETE'])
 def delete_user(userId):
     # this function enables user to delete his/her account
-    for user in users:
-        if user['userId'] == userId:
-            users.remove(user)
-            return jsonify({
-                'status': 200,
-                "message": "account successfully deleted"
-            }), 200
+    user = [user for user in users if user['id'] == userId]
+    if len(user) == 0:
         return jsonify({
             'status': 400,
-            "message": "account not found "}), 400
+            "message": "account not found "
+        }), 400
+    users.remove(user[0])
+    return jsonify({
+        'status': 200,
+        "message": "account successfully deleted"
+    }), 200
 
+@bp.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(userId):
+    user = [user for user in users if user['id'] == userId]
+    if len(user) == 0:
+        return jsonify({
+            'status': 404,
+            'error': 'No record found'
+            }), 404        
+    if not request.json:
+        return jsonify({
+            'status': 404,
+            'error': 'Data should be in json format'
+        }), 400
 
-redflags = [{'flag_Id': 1, 'incidentType': 'redflag', 'comment':'CAO taking bribes', 'location_lat': 23.1225, 'location_long': 0.2325, 'image': 'cao.jpg' , 'video': 'cao.mpgv'},\
-            {'flag_Id': 2, 'incidentType': 'redflag', 'comment':'HR soliciting sexual favours', 'location_lat': 24.1225, 'location_long': 0.2525, 'image': 'df.jpg' , 'video': 'fg.mpgv'}]
-
+    user[0]['firstname'] = request.json.get('firstname', user[0]['firstname'])
+    user[0]['lastname'] = request.json.get('lastname', user[0]['lastname'])
+    user[0]['othernames'] = request.json.get('othernames', user[0]['othernames'])
+    user[0]['email'] = request.json.get('email', user[0]['email'])
+    user[0]['phonenumber'] = request.json.get('phonenumber', user[0]['phonenumber'])
+    return jsonify({'user': user[0]})
+    
 @bp.route('/', methods=['GET'])
 def index():
     return jsonify({
         'IReporter': "On this platform any one can bring any form of corruption or anythning that requires intervation of authorities and the general public."
     }), 200
 
+redflags = [
+    {
+        'id': 1,
+        'type': 'redflag',
+        'comment': u'Buy groceries',
+        'images': u'image1',
+        'videos': u'video1',
+        'location': u'location cordinates',
+        'status': 'draft',
+        "createdOn": "Wed, 02 Jan 2019 15:36:03 GMT"
+    },
+    {
+        'id': 2,
+        'type': 'redflag',
+        'comment': u'Learn Python',
+        'images': u'image2',
+        'videos': u'video2',
+        'location': u'location cordinates',
+        'status': 'draft',
+        "createdOn": "Wed, 02 Jan 2019 16:36:03 GMT"
+    }
+]
 
-@bp.route('/redflag/create', methods=['POST'])
-def addRedflag():    
-    data = request.get_json()    
-    redflag = Redflag(data['flag_Id'], data['incidentType'], data['comment'],
-                    data['location_lat'], data['location_long'], data['image'],
-                    data['video'])
+@bp.route('/redflags', methods=['POST'])
+def create_redflag():    
+    if not request.json or not 'comment' in request.json:
+        return jsonify({
+            'status': 400,
+            'error': 'Bad Request'
+        }), 400
+
+    redflag = {
+        'id': redflags[-1]['id'] + 1,
+        'type': 'redflag',
+        'comment': request.json['comment'],
+        'images': request.json.get('images'),
+        'videos': request.json.get('videos'),
+        'location': request.json.get('location'),
+        'status': 'draft',
+        'createdOn': datetime.now()
+    }
     redflags.append(redflag)
     return jsonify({
         'status': 201,
-        "message": " Successfully created"
+        "message": " Successfully created",
+        'redflag': redflag
     }), 201
 
 
 @bp.route('/redflags', methods=['GET'])
-def returnAll():
+def get_redflags():
     return jsonify({
         'status': 200,
         'redflags':redflags
@@ -105,49 +180,55 @@ def returnAll():
 
 
 @bp.route('/redflags/<int:flag_Id>', methods=['GET'])
-def getflag(flag_Id):
-    for redflag in redflags:
-        if redflag['flag_Id'] == flag_Id:
-            return jsonify({
-                'status': 200,
-                "redflag": redflag
-            }), 200
+def get_redflag(flag_Id):
+    redflag = [redflag for redflag in redflags if redflag['id'] == flag_Id]
+    if len(redflag) == 0:
         return jsonify({
             'status': 400,
             "message": "redflag id provided is out of range "}), 400
+    return jsonify({
+        'status': 200,
+        "redflag": redflag[0]
+    }), 200
+    
 
 @bp.route('/redflags/<int:flag_Id>', methods=['DELETE'])
-def removeRedflag(flag_Id):
-    for redflag in redflags:
-        if redflag['flag_Id'] == flag_Id:
-            redflags.remove(redflag) 
-            return jsonify({
-                'status' : 200,
-                'message':'redflag deleted succesfuly'
-            }), 200
+def delete_redflag(flag_Id):
+    redflag = [redflag for redflag in redflags if redflag['id'] == flag_Id]
+    if len(redflag) == 0:
         return jsonify({
             'status' : 400,
             'error':'redflag not found'
         }), 400 
+    redflags.remove(redflag[0])   
+    return jsonify({
+        'status' : 200,
+        'message':'redflag deleted succesfuly'
+    }), 200
+        
 
 @bp.route('/redflags/<int:flag_Id>', methods=['PUT'])
-def editSpecificFlag(flag_Id):
-    data = request.get_json()
-    for redflag in redflags:
-        if redflag['flag_Id'] == flag_Id:
-            redflag['record_type'] = data['record_type']
-            redflag['comment'] = data['comment']
-            redflag['location_lat'] = data['location_lat']
-            redflag['location_long'] = data['location_long']
-            redflag['image'] = data['image']
-            redflag['video'] = data['video']
+def update_redflag(flag_Id):
+    redflag = [redflag for redflag in redflags if redflag['id'] == flag_Id]
+    if len(redflag) == 0:
         return jsonify({
-            'status': 200,
-            'message': "successfully edited"
-        }), 200
+            'status': 404,
+            'error': 'No record found'
+            }), 404
+    if not request.json:
+        return jsonify({
+            'status': 400,
+            'error': 'Data should be in json format'
+            }), 400
 
+    redflag[0]['type'] = request.json.get('type', redflag[0]['type'])
+    redflag[0]['comment'] = request.json.get('comment', redflag[0]['comment'])
+    redflag[0]['images'] = request.json.get('images', redflag[0]['images'])
+    redflag[0]['videos'] = request.json.get('videos', redflag[0]['videos'])
+    redflag[0]['location'] = request.json.get('location', redflag[0]['location'])
+    redflag[0]['status'] = request.json.get('status', redflag[0]['status'])
     return jsonify({
-            'status': 403,
-            'message': 'operation not allowed'
-        }), 403
-
+        'status': 200,
+        'message': 'successfully edited',
+        'redflag':redflag[0]
+    }), 200
